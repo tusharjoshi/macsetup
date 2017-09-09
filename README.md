4256452452d924b554d4a6af399d657a7c889b43
Tushar Joshi <tusharvjoshi@gmail.com>
2017-09-09 16:11:34 +0530

    Initial Brewfile and macsetup.sh script

---
+++ README.md
@@ -0,0 +1,13 @@
+# macOs setup using Homebrew
+
+## Usage
+
+Copy the script macsetup.sh and Brewfile in $Home directory.
+Run the macsetup.sh script
+
+```bash
+bash macsetup.sh
+```
+
+## Brewfile
+This file contains the tools which I use frequently
---
+++ macsetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Homebrew
+if which brew 2> /dev/null; then
+    brewLocation=`which brew`
+    appLocation=`brew --prefix`
+    echo "Homebrew is installed in $brewLocation"
+    echo "Homebrew apps are run from $appLocation"
+else
+   echo "Installing Homebrew"
+   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+   brew tap "buo/cask-upgrade"
+   brew tap "caskroom/cask"
+   brew tap "homebrew/bundle"
+   brew tap "homebrew/core"
+   brew update
+fi
+
+# Install tools from Brewfile
+
+BREWFILE=$HOME/Brewfile
+
+if [ ! -f "$BREWFILE" ]
+then
+	echo Please create $BREWFILE.
+else
+  brew bundle --file=$BREWFILE
+fi
